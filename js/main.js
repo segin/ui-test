0b2552a5552685b32a099c18d1a03e72dcab306b
Jenni Whitman <jenni@hbs.edu>
2016-03-09 13:24:40 -0500

    added ability to add multiple users separated by semi-colon

--- js/main.js
+++ js/main.js
@@ -9,29 +9,42 @@ function Player(name, score) {
 
 // Add a ranking from form submit
 function addRankings() {
-	// Split the ranking at the comma
-	var splitRanking = document.getElementByID('ranking-entry').value.split(", ");
-	
+	// Split multiple rankings at the semi-colon
+	var splitRanking = document.getElementByID('ranking-entry').value.split('; ');
+
+	// Split each ranking at the comma - name and score
+	for (var i = 0; i < splitRanking.length; i++){
+		var splitRanking[i] = splitRanking[i].split(', ');
+	}
+	// var splitRanking = document.getElementByID('ranking-entry').value.split(", ");
+
 	// Take name and capitalize the first letter
-	var properName = capitalizeName(splitRanking [0]);
+	var properName = "";
+	for (var i = 0; i < splitRanking.length; i++){
+		capitalizeName(splitRanking [i][0]);
+	} 
 
 	// Take string from input after comma and make int
-	var properScore = parseInt[splitRanking [1]];
+	for (var i = 0; i < splitRanking.length; i++){
+		parseInt[splitRanking [i][1]];
+	}
 
 	// Check if user is new or not, add score if not
 	var exists = false;
-	for (var i = 0; i < ranking.length; i++){
-		if(ranking[i].name === properName)
-		{
-			ranking[i].score += score;
-			exists = true;
-			break;
+		for (var i = 0; i < splitRanking.length; i++){
+			for (var j = 0; j < ranking.length; j++){
+				if(ranking[j].name === splitRanking[i][0]){
+					ranking[j].score += splitRanking[i][1];
+					exists = true;
+					break;
+				}
+				if (!exists){
+					var newbie = new Player(name, score);
+					ranking.push(newbie);
+				}
+			}
 		}
-	}
-	if (!exists){
-		var newbie = new Player(name, score);
-		ranking.push(newbie);
-	}
+
 	// Clear work done by adding rankings
 	clearRankingEntry();
     displayRankings();
@@ -43,8 +56,8 @@ function clearRankingEntry() {
     }
 
 // Capitalize the first letter of name in entry - addRanking Helper
-function capitaliseName(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
+function capitalizeName(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 // Hey, Listen! - event listeners
