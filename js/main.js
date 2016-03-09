aaa58c4be8ab900a3353a4816372c425605004b6
Jenni Whitman <jenni@hbs.edu>
2016-03-09 14:03:48 -0500

    before removing semi colon

--- js/main.js
+++ js/main.js
@@ -9,8 +9,13 @@ function Player(name, score) {
 
 // Add a ranking from form submit
 function addRankings() {
+	var splitRanking = [];
+
 	// Split multiple rankings at the semi-colon
-	var splitRanking = document.getElementByID('ranking-entry').value.split('; ');
+	if(document.getElementByID('ranking-entry').value.contains (";")){
+		var splitRanking = document.getElementByID('ranking-entry').value.split('; ');
+	}
+	else var splitRanking[0] = document.getElementByID('ranking-entry').value;
 
 	// Split each ranking at the comma - name and score
 	for (var i = 0; i < splitRanking.length; i++){
@@ -58,6 +63,27 @@ function clearRankingEntry() {
 // Capitalize the first letter of name in entry - addRanking Helper
 function capitalizeName(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function buildRankings() {
+	orderRankings();
+
+	$("#rankings").replaceWith("<ol id=\"rankings\">");
+
+	for (var i = 0; i < ranking.length; i++) {
+		var list = document.getElementById("rankings");
+		var li = document.createElement("li");
+		var scores = ((ranking[i].score == 1) ? "pt" : "pts");
+		var node = document.createTextNode(ranking[i].name + ", " + ranking[i].score + " " + points);
+		list.appendChild(li).appendChild(node);
+	}
+}
+
+// Orders the rankings before creating the list
+function orderRankings() {
+	ranking.sort(functon (a, b) {
+		return b.score - a.score;
+	});
 }
 
 // Hey, Listen! - event listeners
